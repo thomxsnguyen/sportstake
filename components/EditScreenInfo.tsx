d36ebc4e9c1613da41c993bebab3e89a4050ad1a
Thomas <thomasn8255@gmail.com>
2025-05-31 02:35:55 -0700

    setup

--- components/EditScreenInfo.tsx
+++ components/EditScreenInfo.tsx
@@ -7,23 +7,15 @@ export const EditScreenInfo = ({ path }: { path: string }) => {
 
   return (
     <View>
-      <View className={styles.getStartedContainer}>
-        <Text className={styles.getStartedText}>{title}</Text>
-        <View className={styles.codeHighlightContainer + styles.homeScreenFilename}>
+      <View className="mx-12 items-center">
+        <Text className="text-center text-lg leading-6">{title}</Text>
+
+        <View className="my-2 rounded-md px-1">
           <Text>{path}</Text>
         </View>
-        <Text className={styles.getStartedText}>{description}</Text>
+
+        <Text className="text-center text-lg leading-6">{description}</Text>
       </View>
     </View>
   );
-};
-
-const styles = {
-  codeHighlightContainer: `rounded-md px-1`,
-  getStartedContainer: `items-center mx-12`,
-  getStartedText: `text-lg leading-6 text-center`,
-  helpContainer: `items-center mx-5 mt-4`,
-  helpLink: `py-4`,
-  helpLinkText: `text-center`,
-  homeScreenFilename: `my-2`,
 };
