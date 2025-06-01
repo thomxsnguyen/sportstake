8e29d832884d15e1c8334f0a0ef2138592ddcde8
Thomas <thomasn8255@gmail.com>
2025-06-01 12:08:15 -0700

    creating navigation bar

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
-import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
+import { ScreenContent } from 'components/ScreenContent';
+import { SafeAreaView } from 'react-native';
+import { NavigateBar } from 'components/NavigateBar';
 
 import './global.css';
 
 export default function App() {
   return (
-    <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
+    <SafeAreaView className="flex-1">
       <StatusBar style="auto" />
-    </>
+      <NavigateBar />
+      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
+    </SafeAreaView>
   );
 }
---
+++ components/NavigateBar.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export const NavigateBar = () => {
+  return (
+    <View>
+      <Text>NavigateBar</Text>
+    </View>
+  );
+};
